de7b873d95fe3590697fe256bc7b0fa16aabc9cc
Joseph Dempsey <coolfireking02@gmail.com>
2017-03-27 19:56:27 +0100

    Stored channel IDs in variable for easy use, added send message function to keep things organised. Added reddit post broadcasting system using the reddit API, it checks for a new post every 1 minute and publishes it in the #pr channel if one's found. (Warning, the if the bot's restarted it'll post about the newest post it can find, even if it's already been posted)

--- bot.js
+++ bot.js
@@ -1,23 +1,61 @@
+var request = require('request');
 const Discord = require("discord.js");
 const client = new Discord.Client();
+
+var newMemberChannel = 295745377083326464;
+var newRedditPostChannel = 295748944347004948;
+
+//var devChannelID = 270601378341191681;
 
 client.on('ready', () => {
   console.log("Connected!");
 });
 
 client.on('message', msg => {
-  if (msg.content === 'test') {
-    msg.channel.send('Up and running ;D');
+  if (msg.content === 'KONF test') {
+      msg.reply('Up and running! ;D');
   }
 });
 
 client.on('guildMemberAdd', member => {
-   member.guild.channels.forEach(function(channel){
-       if(channel.id == 295745377083326464){
-           channel.sendMessage("Welcome @" + member.user + " to Keep Our Net Free!");
-           console.log("Member " + member.user.username + " joined");
-       }
-   });
+    sendMessage("Welcome " + member.user + " to Keep Our Net Free!", newMemberChannel);
 });
+
+var newestPostTitle = "";
+
+function checkForNewPost () {
+    request('https://www.reddit.com/r/KeepOurNetFree/new/.json', function (error, response, body) {
+        if(error){
+           console.log(error);
+        } else {
+            var postData = JSON.parse(body);
+            var checkPost = postData["data"]["children"][0]["data"].title;
+            var checkPostURL = postData["data"]["children"][0]["data"].url;
+            
+            if(newestPostTitle != checkPost){
+                newestPostTitle = checkPost;
+                sendMessage("New KONF reddit post! \"" + newestPostTitle + "\" " + checkPostURL, newRedditPostChannel);
+            } else {
+                console.log("No new posts D:");
+            }
+        }
+    });
+}
+
+function sendMessage (msg, channelID) {
+    client.guilds.forEach(function(guild){
+        guild.channels.forEach(function(channel){
+           if(channel.id == channelID){
+               channel.sendMessage(msg);
+               console.log("Sent message: " + msg);
+           }
+        });
+    });
+}
+
+var minutes = 1;
+var interval = minutes * 60 * 1000;
+
+setInterval(checkForNewPost, interval);
 
 client.login('Mjk1OTQzNjcxMjk0MDY2Njk5.C7rDCA.M3HrWkkL9MnJ2DuqFGJC1Zy-46E');
