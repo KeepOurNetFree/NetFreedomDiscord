f7ef5bf25a3c641a0091fd2a52af2ca329c0bab5
Joseph Dempsey <coolfireking02@gmail.com>
2017-03-27 17:57:14 +0100

    implemented welcome messages

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("Connected!");
+});
+
+client.on('message', msg => {
+  if (msg.content === 'test') {
+    msg.channel.send('Up and running ;D');
+  }
+});
+
+client.on('guildMemberAdd', member => {
+   member.guild.channels.forEach(function(channel){
+       if(channel.id == 270601378341191681){
+           channel.sendMessage("Welcome @" + member.user + " to Keep Our Net Free!");
+           console.log("Member " + member.user.username + " joined");
+       }
+   });
+});
+
+client.login('Mjk1OTQzNjcxMjk0MDY2Njk5.C7rDCA.M3HrWkkL9MnJ2DuqFGJC1Zy-46E');
