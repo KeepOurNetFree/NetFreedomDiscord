22534178867290af9afda6e93739aef2b982f6fe
Joseph Dempsey <coolfireking02@gmail.com>
2017-03-28 16:43:12 +0100

    Added sub count tracker function

--- bot.js
+++ bot.js
@@ -5,26 +5,29 @@ const client = new Discord.Client();
 var newMemberChannel = 295745377083326464;
 var newRedditPostChannel = 295748944347004948;
 
-//var devChannelID = 270601378341191681;
+var devChannelID = 270601378341191681;
 
 client.on('ready', () => {
   console.log("Connected!");
 });
 
 client.on('message', msg => {
-  if (msg.content === 'KONF test') {
-      msg.reply('Up and running! ;D');
-  }
+    if (msg.content === 'KONF test') {
+        msg.reply('Up and running! ;D');
+    }
+    if (msg.content === 'KONF subscribers') {
+        checkSubCount();
+    }
 });
 
 client.on('guildMemberAdd', member => {
-    sendMessage("Welcome " + member.user + " to Keep Our Net Free!", newMemberChannel);
+    sendMessage("Welcome " + member.user + " to Keep Our Net Free!", devChannelID);
 });
 
 var newestPostTitle = "";
 
 function checkForNewPost () {
-    request('https://www.reddit.com/r/KeepOurNetFree/new/.json', function (error, response, body) {
+    request("https://www.reddit.com/r/KeepOurNetFree/new/.json", function (error, response, body) {
         if(error){
            console.log(error);
         } else {
@@ -34,13 +37,40 @@ function checkForNewPost () {
             
             if(newestPostTitle != checkPost){
                 newestPostTitle = checkPost;
-                sendMessage("New KONF reddit post! \"" + newestPostTitle + "\" " + checkPostURL, newRedditPostChannel);
+                sendMessage("New KONF reddit post! \"" + newestPostTitle + "\" " + checkPostURL, devChannelID);
             } else {
                 console.log("No new posts D:");
             }
         }
     });
 }
+
+var minutes = 1;
+var interval = minutes * 60 * 1000;
+var subCountInterval = (minutes * 30) * 60 * 1000;
+
+setInterval(checkForNewPost, interval);
+
+var currentSubCount = 0;
+
+function checkSubCount () {
+    request("https://www.reddit.com/r/KeepOurNetFree/about.json", function(error, response, body) {
+        if(error){
+            console.log(error);
+        } else {
+            var subData = JSON.parse(body);
+            var subscriberCount = subData["data"].subscribers;
+            
+            if(subscriberCount - currentSubCount >= 1000){
+                sendMessage("Current subscriber count: " + subscriberCount, devChannelID);
+            } else {
+                console.log("Sub change over past 30 minutes: " + subscriberCount - currentSubCount);
+            }
+        } 
+    });
+}
+
+setInterval(checkForNewPost, subCountInterval);
 
 function sendMessage (msg, channelID) {
     client.guilds.forEach(function(guild){
@@ -52,10 +82,5 @@ function sendMessage (msg, channelID) {
         });
     });
 }
-
-var minutes = 1;
-var interval = minutes * 60 * 1000;
-
-setInterval(checkForNewPost, interval);
 
 client.login('');
