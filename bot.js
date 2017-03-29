7d6d849aa8f4f710833b2acf8fe5643cc400c564
Joseph Dempsey <coolfireking02@gmail.com>
2017-03-29 18:12:29 +0100

    Commented on everything, switched some channels around, added some new ones, changed the welcome message

--- bot.js
+++ bot.js
@@ -2,16 +2,26 @@ var request = require('request');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//ID for landing pad
 var newMemberChannel = 295745377083326464;
-var newRedditPostChannel = 295748944347004948;
 
+//ID for subreddit-pr channel
+var newRedditPostChannel = 296414495478382592;
+
+//ID for botdump channel
+var botLoggingChannel = 296674818026373123;
+
+//ID for a channel in my personal dev server when i'm testing locally
 //var devChannelID = 270601378341191681;
 
+//pretty self-explanatory, when the bot's connected properly, log it, for some reason this happens a couple times when the bot first starts
 client.on('ready', () => {
     console.log('Connected!');
 });
 
+//message recieved event
 client.on('message', msg => {
+    //respond to the various commands
     if (msg.content === 'KONF help') {
         msg.reply('New members are welcomed automatically. Run "KONF subscribers" to see the current sub count.');
     }
@@ -20,28 +30,42 @@ client.on('message', msg => {
     }
 });
 
+//when someone joins this event is called, the variable member is of type GuildMember in Discord.JS
 client.on('guildMemberAdd', member => {
-    sendMessage('Welcome ' + member.user + ' to Keep Our Net Free!', newMemberChannel);
+    //welcome the new user
+    sendMessage("Welcome " + member.user + "! Thanks for helping to Keep Our Net Free! Be sure to checkout #info!", newMemberChannel);
+    sendMessage("User " + member.user " has joined.", botLoggingChannel);
+});
+
+//event for people leaving or being kicked, sent to the log only
+client.on('guildMemberRemove', member => {
+    sendMessage("User " + member.user " has left.", botLoggingChannel);
 });
 
 //Current UTC time. Reddit uses seconds, not milliseconds like JS
 var newestPostTimeUTC = Math.floor(new Date().getTime() / 1000);
 
 function checkForNewPost() {
+    //make a request to the api page for sub posts
     request('https://www.reddit.com/r/KeepOurNetFree/new/.json', function (error, response, body) {
         if (error) {
+            //if it errors, log it
             console.log(error);
         }
         else {
             //A try/catch for if the postdata is invalid JSON, if there's no first child, no permalink, etc.
             try {
                 var postData = JSON.parse(body);
+                //get the time, title and url of the newest post, constructed from the JSON
                 var checkPostTimeUTC = postData['data']['children'][0]['data'].created_utc;
                 var checkPostTitle = postData['data']['children'][0]['data'].title;
                 var checkPostURL = 'https://www.reddit.com' + postData['data']['children'][0]['data'].permalink;
 
+                //if the previous post time is older than the one retrieved from the API, something new's been posted
                 if (newestPostTimeUTC < checkPostTimeUTC) {
+                    //set the old post time to the API post time
                     newestPostTimeUTC = checkPostTimeUTC;
+                    //send the discord message and log it
                     sendMessage('New KONF reddit post! "' + checkPostTitle + '" ' + checkPostURL, newRedditPostChannel);
                     console.log('New reddit post! ' + checkPostURL);
                 }
@@ -53,6 +77,7 @@ function checkForNewPost() {
     });
 }
 
+//minutes between checking for a new reddit post
 var minutes = 1;
 var interval = minutes * 60 * 1000;
 
@@ -61,24 +86,31 @@ setInterval(checkForNewPost, interval);
 var currentSubCount = 0;
 
 function checkSubCount() {
+    //make an API request to the about page of the sub
     request('https://www.reddit.com/r/KeepOurNetFree/about.json', function (error, response, body) {
         if (error) {
             console.log(error);
         }
         else {
             var subData = JSON.parse(body);
+            //store the sub count
             var subscriberCount = subData['data'].subscribers;
 
+            //print it out as a reply
             sendMessage('Current subscriber count: ' + subscriberCount, newRedditPostChannel);
             console.log('Current subscriber count: ' + subscriberCount);
         }
     });
 }
 
+//the send message function
 function sendMessage(msg, channelID) {
+    //loop through all guilds the bot's in, currently only 1, the KONF server
     client.guilds.forEach(function (guild) {
+        //in each guild loop through every channel, if it matches the channel we want the message to send to, send it
         guild.channels.forEach(function (channel) {
             if (channel.id == channelID) {
+                //send the message and log it
                 channel.sendMessage(msg);
                 console.log('Sent message: ' + msg);
             }
